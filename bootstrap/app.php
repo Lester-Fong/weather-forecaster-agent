49eae14b55750c7189d9f103544fd3ee18317210
Lester-Fong <lester.fong@switchconnect.com.au>
2025-09-03 06:19:17 +0800

    Refactor: Improve formatting and add comments for CSRF protection in middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Configure CSRF protection
+        $middleware->validateCsrfTokens(except: [
+            // Exclude API routes from CSRF protection
+            'api/weather/query',
+            'api/weather/detect-location',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
